837f145703b277a21b87ea2901475dff2a8570df
Konstantin Baimukhametov <kostik040506@gmail.com>
2025-10-09 21:16:26 +0500

    refactor: Switch TMDb API autentication to Bearer Token

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -1,10 +1,16 @@
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+  },
+}
 
 export async function getMovies() {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`
+  const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`
 
   try {
-    const response = await fetch(url)
+    const response = await fetch(url, options)
     if (!response.ok) {
       throw new Error(`Ошибка: ${response.status}`)
     }
