93a4ffc4b08cf1453a0b04ed577d303ebf9ef000
Konstantin Baimukhametov <kostik040506@gmail.com>
2025-10-10 16:40:32 +0500

    feat: add TMDb configuration fetching

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -22,3 +22,20 @@ export async function getMovies() {
     return null
   }
 }
+
+//Запрос к конфигурациям img
+async function getConfigurations() {
+  const url = `https://api.themoviedb.org/3/configuration`
+
+  try {
+    const response = await fetch(url, options)
+    if (!response.ok) {
+      throw new Error(`Ошибка: ${response.status}`)
+    }
+    const configurationData = await response.json()
+    return configurationData
+  } catch (error) {
+    console.error('Ошибка при получении конфигураций', error)
+  }
+}
+console.log(getConfigurations())
